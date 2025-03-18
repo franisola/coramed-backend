98b35e594a7dccdd6b8772ff9d9464d9888057c6
Francisco Isola <franciscoisola1@gmail.com>
2025-03-18 20:39:05 -0300

    Subiendo estructura

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
+const app = express();
+export default app;
+
+// Midlewares
+// app.use(cors(
+//     {
+//         origin: 'http://localhost:5173',
+//         credentials: true,
+//     }
+// ));
+
+// app.use(morgan('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+
+
+// Routes
+
+
+app.use((err, req, res, next) => {
+	const statusCode = err.statusCode || 500;
+	const error = err || 'Internal Server Error';
+	return res.status(statusCode).json({
+		success: false,
+		error,
+		statusCode,
+	});
+});
