0c9912c03c6ccb5e72fe8ed988b88dfc2e6c6e5f
Francisco Isola <franciscoisola1@gmail.com>
2025-03-21 15:19:07 -0300

    Port

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,13 @@
 import app from './app.js';
 import connectDB from './db.js';
 
+const PORT = process.env.PORT || 3000; // Usa el puerto proporcionado por Railway o 3000 como predeterminado
+
 const startServer = async () => {
   try {
     await connectDB(); // Conectar a la base de datos
-    app.listen(3002, () => {
-      console.log('🚀 Servidor corriendo en el puerto 3002');
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
     });
   } catch (error) {
     console.error('❌ Error al iniciar el servidor:', error.message);
