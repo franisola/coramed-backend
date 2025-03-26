ba66fdb78816a55cc10b199ae1059370fe244d39
Francisco <franciscoisola1@gmail.com>
2025-03-26 11:47:49 -0300

    mensaje de error

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,6 +6,7 @@ export const me = async (req, res, next) => {
 	try {
 		return res.status(200).json({message: 'User found'});
 	} catch (error) {
+		return res.status(500).json({message: 'Internal Server Error'});
 		next(error);
 	}
 }
