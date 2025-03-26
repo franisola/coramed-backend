4bd3df5c5013ccf456cc458d90dcf516cacf4cc7
Francisco <franciscoisola1@gmail.com>
2025-03-26 11:41:59 -0300

    corrigeiendo mas coisas

--- src/middlewares/validate.js
+++
@@ -1,8 +0,0 @@
-export const authNotRequired = (req, res, next) => {
-	const { token } = req.cookies;
-
-
-	if (token) return res.status(401).json({ message: 'Authorization denied' });
-
-	next();
-};
