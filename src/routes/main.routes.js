d9bc585f67e438c9916cedc04f9161557666ca06
Francisco Isola <franciscoisola1@gmail.com>
2025-05-29 12:49:41 -0300

    otro detalle

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -4,44 +4,15 @@ import healthInsuranceRoutes from "./healthInsurance.routes.js";
 import medicalInfoRoutes from "./medicalInfo.routes.js";
 import authRoutes from "./auth.routes.js";
 import appointmentRoutes from "./appointment.routes.js";
-import professionalRoutes from "./professional.routes.js"; 
+import professionalRoutes from "./professional.routes.js";
 
 const router = Router();
 
-/**
- * @route /auth
- * @desc Authentication routes
- */
 router.use("/auth", authRoutes);
-
-/**
- * @route /user
- * @desc User routes
- */
 router.use("/user", userRoutes);
-
-/**
- * @route /health-insurance
- * @desc Health insurance routes
- */
 router.use("/health-insurance", healthInsuranceRoutes);
-
-/**
- * @route /medical-info
- * @desc Medical information routes
- */
 router.use("/medical-info", medicalInfoRoutes);
-
-/**
- * @route /appointments
- * @desc Appointment routes
- */
 router.use("/appointments", appointmentRoutes);
-
-/**
- * @route /professionals
- * @desc Professional routes
- */
 router.use("/professionals", professionalRoutes);
 
 export default router;
